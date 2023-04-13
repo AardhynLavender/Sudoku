773f9eeb809ba4d68e2de7c1dd25267787e11668
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-04-13 16:39:35 +1200

    build: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sudoku
+
+> A digital Sudoku client and solver
---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Title } from "solid-start";
+
+export default function App() {
+  return (
+    <main>
+      <Title>Sudoku</Title>
+      <h1>Sudoku</h1>
+      <p>Coming soon...</p>
+    </main>
+  );
+}
